7d9ed117936298f09e18e4df70097a8206556e56
Adam Fisher <amgfisher90@gmail.com>
2022-12-19 12:55:47 -0500

    commit tree b4d8e4c9f2e45f249f0bd529111334f53159e738

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(event) {
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>Keypad
+            <input type="password" onChange={handleChange}/>
+        </div>
     )
 }
 
